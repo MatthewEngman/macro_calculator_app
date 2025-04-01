d928c27384882b98f568b3f69d5b6eebe5c3691a
MatthewEngman <matt.engman@gmail.com>
2025-04-01 15:34:32 -0400

    still playing with splash screen

--- android/app/src/main/kotlin/com/example/macro_calculator/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/macro_calculator/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.example.macro_calculator
 
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Install the splash screen but let Flutter control its removal
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+    }
+}
