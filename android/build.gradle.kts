b63988efb2498cd1c1b7e9597107523a259a1ef9
MatthewEngman <matt.engman@gmail.com>
2025-04-10 15:32:41 -0400

    gradle update

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -30,7 +30,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.4.0-alpha09")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
         classpath("com.google.gms:google-services:4.4.2")
     }
 }
