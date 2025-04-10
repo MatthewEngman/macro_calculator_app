7686186d4208196c398ef219abe8f2879f636e4b
MatthewEngman <matt.engman@gmail.com>
2025-04-10 16:44:03 -0400

    fixing security issue

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
             create("release") {
                 keyAlias = keystoreProperties["keyAlias"] as String
                 keyPassword = keystoreProperties["keyPassword"] as String
-                storeFile = file(keystoreProperties["storeFile"] as String)
+                storeFile = file(keystoreProperties["storeFile"].toString().replace("android/", ""))
                 storePassword = keystoreProperties["storePassword"] as String
             }
         }
